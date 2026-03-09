9cd4b86594ff29c381ee544cb8740365aada4b55
shi1617 <2064645693@qq.com>
2026-03-09 18:18:53 +0800

    Initial commit

---
+++ src/main/java/com/stu/hellosever/HelloSeverApplication.java
@@ -0,0 +1,13 @@
+package com.stu.hellosever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSeverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSeverApplication.class, args);
+    }
+
+}
